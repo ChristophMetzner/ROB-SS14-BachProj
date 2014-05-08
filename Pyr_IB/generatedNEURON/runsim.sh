8e111c4cef647ebd918dcc2cca66eac7be52c251
NN BP2 <bp2@gw10.(none)>
2014-05-08 11:00:48 +0200

    Initial commit 2

---
+++ Pyr_IB/generatedNEURON/runsim.sh
@@ -0,0 +1,2 @@
+cd '/home/kloskowski/Desktop/Python-skript/Pyr_IB/simulations/multiCurrent_2'
+/home/kloskowski/Desktop/Python-skript/Pyr_IB/simulations/multiCurrent_2/x86_64/special Pyr_IB.hoc > /tmp/logNEURON_Pyr_IB.ncx_multiCurrent_2
