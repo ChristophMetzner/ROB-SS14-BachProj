62e4f6b91680c5338ce237b498f25cb0bef45ac1
Felix Esch <felix_esch@alice.de>
2014-07-11 01:57:25 +0200

    Added plot.py for displaying neurosim.py results.

--- plot.py
+++ plot.py
@@ -14,20 +14,22 @@ import matplotlib.pyplot as plt
 from nevo.util import projconf
 
 def plot(pconf, simulations, time = 1.0):
-    time = min(time, 1.0)
+    dt = pconf.get_float("dt", "Simulation")
+    Fs = 1000 / dt
+    Ts = 1.0 / Fs; # sampling interval
+    duration = pconf.get_float("duration", "Simulation")
+    time = min(time, duration / 1000)
+    t = numpy.arange(0, time, Ts) # time vector
     D = []
     for simulation in simulations:
         densityFile = projconf.norm_path(pconf.get_sim_project_path(),
                                          "simulations", simulation, "CellGroup_1_0.dat")
-        Fs = 1000 / pconf.get_float("dt", "Simulation");  # sampling rate 1000ms/dt
-        Ts = 1.0 / Fs; # sampling interval
-        t = numpy.arange(0, time, Ts) # time vector
         with open(densityFile,'r') as fileDE:
             density = numpy.zeros(len(t))
             densities_list= fileDE.read().split('#\n')      
             densities = densities_list[0].split('\n')
             for i in range(len(density)):
-                dens = densities[i].strip()             
+                dens = densities[i].strip()
                 density[i] = float(dens)
         fileDE.close()
         y = density
@@ -51,13 +53,13 @@ def main():
     parser.add_argument(metavar = "SIM_PATH", dest = "sim_path",
                         help = """Path to the simulation path containing the config files used and the neuroConstruct project.
                         This is also called simulation path and the result of calling "start_sim".""")
-    parser.add_argument("-s", "--simulation", action = "append",
+    parser.add_argument("-s", "--simulation", required = True, action = "append",
                         help = """Just the name of the simulation as
                         found in the neuroConstruct's neuron simulation.
                         Example: Use "PySim_27" for "Pyr_RS/simulations/PySim_27" """)
     parser.add_argument("-t", "--time", type = float, default = 1.0,
                         help = """The time in seconds that should be displayed.
-                        Only valid values are between 0.0 and 1.0.""")
+                        Valid values depend on sampling size and are usually between 0 and 1.0.""")
     options = parser.parse_args()
 
     configFile = projconf.norm_path(options.sim_path, "full.cfg")
