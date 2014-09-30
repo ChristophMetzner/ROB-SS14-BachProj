ChristophMetzner/ROB-SS14-BachProj

commit a34aa4c86fa0f36945e6ac8d882b08c12ff61e36

Added comments and deleted code that had only testing purposes

BachProjGui.py
@@ -156,7 +156,7 @@ class SampleApp(tk.Tk):
         frame = self.frames[c]
         frame.tkraise()
 
-
+    #load the chosen parameter in the config file and start the specified algorithm
     def start(self):
         global gconfig
 
@@ -301,30 +301,6 @@ class ChooseAlgoPage(tk.Frame):
         tk.Frame.__init__(self, parent)
         
         
-        
-        
-        #For testing puposes only
-        def debugActivated():
-            print controller.debugValue.get()
-
-        def threadsget():
-            print controller.ThreadsValue.get()
-            
-        def fileLogLevelget():
-            print controller.filelogValue.get()
-            
-        
-        def consoleLogLevelget():
-            print controller.consolelogValue.get()
-
-        
-        def modeget():
-               selection = "You selected the option " + str(modeVar.get())
-               print selection
-               print configIsLoaded
-               
-
-        #TODO: noch wird nicht alle geladen
         def loadConfigFileDialog():
             name= askopenfilename() 
             loadConfigFile(name)
@@ -669,7 +645,6 @@ class ChooseAlgoPage(tk.Frame):
 # Page to set the parameter for the genetic algorithm and start the program
 
     # choosen values of the parameters are saved in
-    #
     # modeVar
     # popSizeValue
     # MaxGenerationValue
@@ -697,23 +672,6 @@ class ChooseAlgoPage(tk.Frame):
 class EvoluParameterPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-
-
-        
-
-        def tournamentselection():
-            print ""
-            
-
-        def fitnessproportionate():
-            print""
-
-        def n_point_crossover():
-            print""
-
-        def nuMutation():
-            print""
-        
 
         
         label = tk.Label(self, text="Set Parameter for the Genetic Algorithm", font=TITLE_FONT)
@@ -892,7 +850,7 @@ class EvoluParameterPage(tk.Frame):
     # start_temperatureValue
     # cooling_scheduleValue
     # If cooling_scheduleValue = 2 which means exponential
-    #   then cooling_schedule_alphaValue has to read too
+    # then cooling_schedule_alphaValue has to read too
     
 
 class SimulatedAnnealingPage(tk.Frame):
@@ -995,6 +953,7 @@ class SimulatedAnnealingPage(tk.Frame):
         
     # choosen values of the parameters are saved in
     # gridmodeValue
+    # different channels
 
 
 class GridsearchPage(tk.Frame):
@@ -1003,6 +962,7 @@ class GridsearchPage(tk.Frame):
         label = tk.Label(self, text="Set Parameter for the Gridsearch", font=TITLE_FONT)
         label.pack(side="top", fill="x", pady=10)
         
+        #Button to get back to the startpage
         button = tk.Button(self, text="Go to the start page", 
                            command=lambda: controller.show_frame(ChooseAlgoPage))
         button.pack()

commit b1728b285865c5fa1b8e275a4a5384163eefecdd

Fixed undefined name errors caused by a34aa4c

BachProjGui.py
@@ -292,7 +292,6 @@ class ChooseAlgoPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         
-        
         def loadConfigFileDialog():
             name= askopenfilename() 
             loadConfigFile(name)
@@ -522,8 +521,7 @@ class ChooseAlgoPage(tk.Frame):
         globalParaFrame.pack(fill="both", expand="yes")
 
         debugLabel = tk.Label(globalParaFrame, text="Debug Mode?")
-        debugCheckButton = tk.Checkbutton(globalParaFrame, variable = controller.debugValue, onvalue = 1, offvalue = 0,
-                                          command=debugActivated)
+        debugCheckButton = tk.Checkbutton(globalParaFrame, variable = controller.debugValue, onvalue = 1, offvalue = 0)
         debugLabel.pack()
         debugCheckButton.pack()
 
@@ -540,17 +538,13 @@ class ChooseAlgoPage(tk.Frame):
         filelogFrame = tk.Frame(globalParaFrame)
         filelogFrame.pack()
         
-        AnythingFile = tk.Radiobutton(filelogFrame, text="Anything", variable=controller.filelogValue, value=1,
-                          command=fileLogLevelget)
+        AnythingFile = tk.Radiobutton(filelogFrame, text="Anything", variable=controller.filelogValue, value=1)
         AnythingFile.grid(row =0, column=0)
-        INFOFile = tk.Radiobutton(filelogFrame, text="Infos", variable=controller.filelogValue, value=20,
-                          command=fileLogLevelget)
+        INFOFile = tk.Radiobutton(filelogFrame, text="Infos", variable=controller.filelogValue, value=20)
         INFOFile.grid(row =0, column=1)
-        WARNINGFile = tk.Radiobutton(filelogFrame, text="Warnings", variable=controller.filelogValue, value=30,
-                          command=fileLogLevelget)
+        WARNINGFile = tk.Radiobutton(filelogFrame, text="Warnings", variable=controller.filelogValue, value=30)
         WARNINGFile.grid(row =0, column=2)
-        CRITICALFile = tk.Radiobutton(filelogFrame, text="Criticals", variable=controller.filelogValue, value=50,
-                          command=fileLogLevelget)
+        CRITICALFile = tk.Radiobutton(filelogFrame, text="Criticals", variable=controller.filelogValue, value=50)
         CRITICALFile.grid(row =0, column=3)
 
         
@@ -561,17 +555,13 @@ class ChooseAlgoPage(tk.Frame):
         consolelogFrame= tk.Frame(globalParaFrame)
         consolelogFrame.pack()
 
-        AnythingConsole = tk.Radiobutton(consolelogFrame, text="Anything", variable=controller.consolelogValue, value=1,
-                          command=consoleLogLevelget)
+        AnythingConsole = tk.Radiobutton(consolelogFrame, text="Anything", variable=controller.consolelogValue, value=1)
         AnythingConsole.grid(row =0, column=0)
-        INFOConsole = tk.Radiobutton(consolelogFrame, text="Infos", variable=controller.consolelogValue, value=20,
-                          command=consoleLogLevelget)
+        INFOConsole = tk.Radiobutton(consolelogFrame, text="Infos", variable=controller.consolelogValue, value=20)
         INFOConsole.grid(row =0, column=1)
-        WARNINGConsole = tk.Radiobutton(consolelogFrame, text="Warnings", variable=controller.consolelogValue, value=30,
-                          command=consoleLogLevelget)
+        WARNINGConsole = tk.Radiobutton(consolelogFrame, text="Warnings", variable=controller.consolelogValue, value=30)
         WARNINGConsole.grid(row =0, column=2)
-        CRITICALConsole = tk.Radiobutton(consolelogFrame, text="Criticals", variable=controller.consolelogValue, value=50,
-                          command=consoleLogLevelget)
+        CRITICALConsole = tk.Radiobutton(consolelogFrame, text="Criticals", variable=controller.consolelogValue, value=50)
         CRITICALConsole.grid(row =0, column=3)
 
         
@@ -585,17 +575,13 @@ class ChooseAlgoPage(tk.Frame):
         kindOfNeuronsFrame = tk.Frame(SimulationFrame)
         kindOfNeuronsFrame.pack()
         
-        RS = tk.Radiobutton(kindOfNeuronsFrame, text="Regular Spiking", variable=controller.modeVar, value="RS",
-                          command=modeget)
+        RS = tk.Radiobutton(kindOfNeuronsFrame, text="Regular Spiking", variable=controller.modeVar, value="RS")
         RS.grid(row =0, column=0)
-        FS = tk.Radiobutton(kindOfNeuronsFrame, text="Fast Spiking", variable=controller.modeVar, value="FS",
-                          command=modeget)
+        FS = tk.Radiobutton(kindOfNeuronsFrame, text="Fast Spiking", variable=controller.modeVar, value="FS")
         FS.grid(row =0, column=1)
-        IB = tk.Radiobutton(kindOfNeuronsFrame, text="Intrinsic Bursting", variable=controller.modeVar, value="IB",
-                          command=modeget)
+        IB = tk.Radiobutton(kindOfNeuronsFrame, text="Intrinsic Bursting", variable=controller.modeVar, value="IB")
         IB.grid(row =0, column=2)
-        CH = tk.Radiobutton(kindOfNeuronsFrame, text="Chattering Bursting", variable=controller.modeVar, value="CH",
-                          command=modeget)
+        CH = tk.Radiobutton(kindOfNeuronsFrame, text="Chattering Bursting", variable=controller.modeVar, value="CH")
         CH.grid(row =0, column=3)
 
         